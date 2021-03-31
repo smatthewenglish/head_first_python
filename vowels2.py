6e783ce541d5462fb2f84cc901c713fcf5895240
smatthewenglish <s.matthew.english@gmail.com>
2021-03-31 12:41:14 -0400

    vowels2

---
+++ vowels2.py
@@ -0,0 +1,14 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+#word = "Milliways"
+word = input("Provide a word to search for vowels: ")
+
+found = []
+
+for letter in word:
+    if letter in vowels:
+        #print(letter)
+        if letter not in found:
+        	found.append(letter)
+for vowel in found:
+	print(vowel)
